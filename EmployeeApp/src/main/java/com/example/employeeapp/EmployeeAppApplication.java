ed78525669f9aee59f1e959f79eb85c09869fbeb
YashG0728 <yashgharde07@gmail.com>
2022-12-01 01:52:06 +0530

    Added: Functionality to introducing dto

--- EmployeeApp/src/main/java/com/example/employeeapp/EmployeeAppApplication.java
+++ EmployeeApp/src/main/java/com/example/employeeapp/EmployeeAppApplication.java
@@ -1,13 +1,13 @@
-package com.example.employeepayrollapp;
+package com.example.employeeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeePayrollAppApplication {
+public class EmployeeAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        SpringApplication.run(EmployeeAppApplication.class, args);
     }
 
 }
--- EmployeeApp/src/test/java/com/example/employeeapp/EmployeeAppApplicationTests.java
+++ EmployeeApp/src/test/java/com/example/employeeapp/EmployeeAppApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.employeepayrollapp;
+package com.example.employeeapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EmployeePayrollAppApplicationTests {
+class EmployeeAppApplicationTests {
 
     @Test
     void contextLoads() {
